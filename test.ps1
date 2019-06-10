8853fc24ccacbd71d617550411687fa2064def99
Juan Carlos Perez Amin <juancarlosperezamin@yahoo.com>
2019-06-10 13:47:55 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,6 +2,7 @@ New-Item -Path 'C:\temp\jc' -ItemType Directory
 Get-Disk |
 Where-Object PartitionStyle -eq 'RAW' |
 Initialize-Disk -PartitionStyle MBR -PassThru |
-New-Partition –DiskNumber 2 -Size 200gb -DriveLetter E |
-Format-Volume -DriveLetter E -FileSystem NTFS -Confirm:$false
-New-Item -Path 'E:\MWS' -ItemType Directory
+New-Partition -AssignDriveLetter -UseMaximumSize |
+Format-Volume -FileSystem NTFS -Confirm:$false
+Get-Partition -DiskNumber 2 | Set-Partition -NewDriveLetter G
+New-Item -Path 'G:\MWS' -ItemType Directory
