5f793f8fe4e53fe151a2d43965e7923b9f683457
Juan Carlos Perez Amin <juancarlosperezamin@yahoo.com>
2019-06-10 18:31:14 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -6,3 +6,5 @@ New-Partition -AssignDriveLetter -UseMaximumSize |
 Format-Volume -FileSystem NTFS -Confirm:$false
 Get-Partition -DiskNumber 2 | Set-Partition -NewDriveLetter G
 New-Item -Path 'G:\MWS' -ItemType Directory
+#Disable Automatic Windows Updates
+Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU -Name AUOptions -Value 1
