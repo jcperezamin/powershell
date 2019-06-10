af11508668a6a94b9d0c8a454e258f7329e34a33
Juan Carlos Perez Amin <juancarlosperezamin@yahoo.com>
2019-06-10 12:54:56 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,6 @@
 New-Item -Path 'C:\temp\jc' -ItemType Directory
+Get-Disk |
+Where-Object PartitionStyle -eq 'RAW' |
+Initialize-Disk -PartitionStyle MBR -PassThru |
+New-Partition -AssignDriveLetter -UseMaximumSize |
+Format-Volume -FileSystem NTFS -Confirm:$false
