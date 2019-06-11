5cefce24e13ef44dc2583825959fcb83f3a21909
Juan Carlos Perez Amin <juancarlosperezamin@yahoo.com>
2019-06-11 09:54:20 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -5,6 +5,7 @@ Initialize-Disk -PartitionStyle MBR -PassThru |
 New-Partition -AssignDriveLetter -UseMaximumSize |
 Format-Volume -FileSystem NTFS -Confirm:$false
 Get-Partition -DiskNumber 2 | Set-Partition -NewDriveLetter G
+Set-Volume -DriveLetter G -NewFileSystemLabel "IFS_HOME"
 New-Item -Path 'G:\MWS' -ItemType Directory
 #Disable Automatic Windows Updates
 Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU -Name AUOptions -Value 1
