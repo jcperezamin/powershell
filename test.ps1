486383d59c4db06f3ca97849353d3c311aef6070
Juan Carlos Perez Amin <juancarlosperezamin@yahoo.com>
2019-06-11 11:00:04 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -3,8 +3,8 @@ $drv = Get-WmiObject win32_volume -filter 'DriveLetter = "E:"'
 $drv.DriveLetter = "Z:"
 $drv.Put() | out-null
 New-Item -Path 'C:\temp\jc' -ItemType Directory
-Initialize-Disk 2 –PartitionStyle MBR
+Initialize-Disk 2 -PartitionStyle MBR
 Get-Disk -Number 2 | New-Partition -UseMaximumSize -DriveLetter E | Format-Volume -FileSystem NTFS -NewFileSystemLabel "IFS_HOME" -Confirm:$False 
 New-Item -Path 'E:\IFS\InstanceName\MWS' -ItemType Directory
 #Disable Automatic Windows Updates
-Add-MpPreference -ExclusionPath "E:\IFS\InstanceNam
+Add-MpPreference -ExclusionPath "E:\IFS\InstanceName\MWS"
