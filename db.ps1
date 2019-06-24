6856ff220c78d64297c13c9076e5c8f7a78772b3
Juan Carlos Perez Amin <juancarlosperezamin@yahoo.com>
2019-06-24 12:42:34 +0100

    Update db.ps1

--- db.ps1
+++ db.ps1
@@ -1,14 +1,14 @@
 #Change CD drive letter
-$drv = Get-WmiObject win32_volume -filter 'DriveLetter = "E:"'
-$drv.DriveLetter = "Z:"
-$drv.Put() | out-null
-New-Item -Path 'C:\temp\jc' -ItemType Directory
-Initialize-Disk 2 -PartitionStyle MBR
-Initialize-Disk 3 -PartitionStyle MBR
-Get-Disk -Number 2 | New-Partition -UseMaximumSize -DriveLetter E | Format-Volume -FileSystem NTFS -NewFileSystemLabel "ORADATA" -Confirm:$False
-Get-Disk -Number 3 | New-Partition -UseMaximumSize -DriveLetter F | Format-Volume -FileSystem NTFS -NewFileSystemLabel "ORABKP" -Confirm:$False 
-New-Item -Path 'E:\Oracle' -ItemType Directory
-New-Item -Path 'F:\Oracle\FRA' -ItemType Directory
+#$drv = Get-WmiObject win32_volume -filter 'DriveLetter = "E:"'
+#$drv.DriveLetter = "Z:"
+#$drv.Put() | out-null
+#New-Item -Path 'C:\temp\jc' -ItemType Directory
+#Initialize-Disk 2 -PartitionStyle MBR
+#Initialize-Disk 3 -PartitionStyle MBR
+#Get-Disk -Number 2 | New-Partition -UseMaximumSize -DriveLetter E | Format-Volume -FileSystem NTFS -NewFileSystemLabel "ORADATA" -Confirm:$False
+#Get-Disk -Number 3 | New-Partition -UseMaximumSize -DriveLetter F | Format-Volume -FileSystem NTFS -NewFileSystemLabel "ORABKP" -Confirm:$False 
+#New-Item -Path 'E:\Oracle' -ItemType Directory
+#New-Item -Path 'F:\Oracle\FRA' -ItemType Directory
 #Disable Automatic Windows Updates
 Add-MpPreference -ExclusionPath "E:\Oracle"
 Add-MpPreference -ExclusionPath "E:\Oracle\FRA"
